a1c73087ffd6de265ca8e2a1f87f384aeb72c251
Adam Raine <6752989+adamraine@users.noreply.github.com>
2023-09-01 16:08:44 -0700

    tests(dbw): remove upper bound for LCP load start/end (#15432)

--- cli/test/smokehouse/test-definitions/dobetterweb.js
+++ cli/test/smokehouse/test-definitions/dobetterweb.js
@@ -577,8 +577,8 @@ const expectations = {
         _excludeRunner: 'devtools',
         details: {items: {0: {
           timeToFirstByte: '450+/-100',
-          lcpLoadStart: '7750+/-1000',
-          lcpLoadEnd: '7750+/-1000',
+          lcpLoadStart: '>5000',
+          lcpLoadEnd: '>5000',
         }}},
       },
       'largest-contentful-paint-element': {
