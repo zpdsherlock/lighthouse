f47b3794f23458b7d9fec473b53b1d2832c2bb9d
Adam Raine <6752989+adamraine@users.noreply.github.com>
2023-09-08 12:13:48 -0700

    tests(devtools): sync e2e (#15444)

--- third-party/devtools-tests/e2e/lighthouse/navigation_test.ts
+++ third-party/devtools-tests/e2e/lighthouse/navigation_test.ts
@@ -13,7 +13,7 @@ import {
   waitFor,
   waitForElementWithTextContent,
 } from '../../shared/helper.js';
-import {describe, it} from '../../shared/mocha-extensions.js';
+import {describe} from '../../shared/mocha-extensions.js';
 import {
   clickStartButton,
   getAuditsBreakdown,
@@ -82,10 +82,17 @@ describe('Navigation', async function() {
     ]);
 
     let numNavigations = 0;
-    const {target} = await getBrowserAndPages();
-    target.on('framenavigated', () => ++numNavigations);
+    const {target, frontend} = getBrowserAndPages();
+    target.on('framenavigated', () => {
+      ++numNavigations;
+      if (numNavigations === 6) {
+        void frontend.bringToFront();
+      }
+    });
 
     await clickStartButton();
+
+    await target.bringToFront();
 
     const {lhr, artifacts, reportEl} = await waitForResult();
 
@@ -125,7 +132,6 @@ describe('Navigation', async function() {
       'document-title',
       'html-has-lang',
       'meta-description',
-      'bf-cache',
     ]);
 
     const viewTraceButton = await $textContent('View Trace', reportEl);
@@ -187,6 +193,9 @@ describe('Navigation', async function() {
 
     await clickStartButton();
 
+    const {target} = getBrowserAndPages();
+    await target.bringToFront();
+
     const {lhr, reportEl} = await waitForResult();
 
     assert.strictEqual(lhr.configSettings.throttlingMethod, 'devtools');
@@ -208,7 +217,6 @@ describe('Navigation', async function() {
       'document-title',
       'html-has-lang',
       'meta-description',
-      'bf-cache',
     ]);
 
     const viewTraceButton = await $textContent('View Trace', reportEl);
@@ -225,6 +233,9 @@ describe('Navigation', async function() {
     await selectDevice('desktop');
 
     await clickStartButton();
+
+    const {target} = getBrowserAndPages();
+    await target.bringToFront();
 
     const {reportEl, lhr, artifacts} = await waitForResult();
 
