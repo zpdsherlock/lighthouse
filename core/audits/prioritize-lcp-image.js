138eaf000200e703d34011daa72bc0c32c5ee242
Adriana Ixba <ixbaadriana@gmail.com>
2024-01-08 14:02:01 -0800

    core: bump guidance level of top CWV recommendations (#15695)

--- core/audits/prioritize-lcp-image.js
+++ core/audits/prioritize-lcp-image.js
@@ -37,7 +37,7 @@ class PrioritizeLcpImage extends Audit {
       title: str_(UIStrings.title),
       description: str_(UIStrings.description),
       supportedModes: ['navigation'],
-      guidanceLevel: 3,
+      guidanceLevel: 4,
       requiredArtifacts: ['traces', 'devtoolsLogs', 'GatherContext', 'URL', 'TraceElements'],
       scoreDisplayMode: Audit.SCORING_MODES.METRIC_SAVINGS,
     };
