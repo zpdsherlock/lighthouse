d1cf7e1039f53822611bd3839d5cce34323fac6d
Adriana Ixba <ixbaadriana@gmail.com>
2024-01-08 13:57:29 -0800

    core: lower guidance level of unused- audits (#15718)

--- core/audits/byte-efficiency/unused-css-rules.js
+++ core/audits/byte-efficiency/unused-css-rules.js
@@ -33,7 +33,7 @@ class UnusedCSSRules extends ByteEfficiencyAudit {
       title: str_(UIStrings.title),
       description: str_(UIStrings.description),
       scoreDisplayMode: ByteEfficiencyAudit.SCORING_MODES.METRIC_SAVINGS,
-      guidanceLevel: 2,
+      guidanceLevel: 1,
       requiredArtifacts: ['CSSUsage', 'URL', 'devtoolsLogs', 'traces', 'GatherContext'],
     };
   }
