fbf438a547183ebbd3372bc36e0255dde8538864
Connor Clark <cjamcl@google.com>
2024-02-26 15:28:10 -0800

    core(network-request): use rendererStartTime for initiator candidates (#15832)

--- core/lib/network-recorder.js
+++ core/lib/network-recorder.js
@@ -257,7 +257,7 @@ class NetworkRecorder extends RequestEventEmitter {
     let candidates = recordsByURL.get(initiatorURL) || [];
     // The (valid) initiator must come before the initiated request.
     candidates = candidates.filter(c => {
-      return c.responseHeadersEndTime <= record.networkRequestTime &&
+      return c.responseHeadersEndTime <= record.rendererStartTime &&
           c.finished && !c.failed;
     });
     if (candidates.length > 1) {
