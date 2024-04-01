5827da99b0a259cc0315bb0a4adc1c5b2a6da7e4
Connor Clark <cjamcl@google.com>
2024-04-01 11:31:01 -0700

    tests: remove Access-Control-Allow-Origin for robots.txt (#15895)

--- cli/test/fixtures/static-server.js
+++ cli/test/fixtures/static-server.js
@@ -109,6 +109,12 @@ class Server {
         'Origin-Agent-Cluster': '?1',
       };
 
+      // This enables an important test in Smokerider - to check if the universal fetcher
+      // used for robots.txt (and source maps) is able to fetch freely while ignoring CORS constraints.
+      if (filePath === '/robots.txt') {
+        delete headers['Access-Control-Allow-Origin'];
+      }
+
       const contentType = mime.lookup(filePath);
       const charset = mime.lookup(contentType);
       // `mime.contentType` appends the correct charset too.
